c9a44ccdf1c12e52606ab744af076941e87303fc
Raphael Maia <raphaelmaia@r81.com.br>
2015-12-15 12:50:17 -0200

    code updated

--- inc/contato.php
+++ inc/contato.php
@@ -2,14 +2,62 @@
 <?php
 if (isset($_POST['hidden']) && is_string($_POST['hidden'])):
 
-    $Name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
-    $Email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-    $Msg = filter_input(INPUT_POST, 'msg', FILTER_SANITIZE_EMAIL);
+    // Inicializa o array
+    $empty = [];
 
-    echo
+    if (empty(filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING))):
 
-    'Recebemos seu contato com sucesso!<br>'
-    . $Name . ' - ' . $Email . '<br>' . $Msg;
+        $empty[] = 'Preencha o Campo Nome';
+
+    else:
+
+        $Name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+
+    endif;
+
+    if (empty(filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING))):
+
+        $empty[] = 'Preencha o Campo Email';
+
+    else:
+
+        $Email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+
+    endif;
+
+    if (empty(filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_STRING))):
+
+        $empty[] = 'Preencha o Campo Assunto';
+
+    else:
+
+        $Subject = filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_STRING);
+
+    endif;
+
+    if (empty(filter_input(INPUT_POST, 'msg', FILTER_SANITIZE_STRING))):
+
+        $empty[] = 'Preencha o Campo Mensagem';
+
+    else:
+
+        $Msg = filter_input(INPUT_POST, 'msg', FILTER_SANITIZE_STRING);
+
+    endif;
+
+    if (empty($empty)):
+
+        echo "<h3 class='text-center'>Recebemos seu contato com sucesso!</h3>" . '<p>Dados:</p>' . $Name . '<br>' . $Email . '<br>' . $Subject . '<br>' . $Msg;
+
+    else:
+
+        foreach ($empty as $key => $value) {
+
+            echo "<p class='text-center'>{$value}</p>";
+        }
+
+    endif;
+
 endif;
 ?>
 
@@ -17,13 +65,19 @@ endif;
     <div class="form-group">
         <label for="nome" class="col-sm-2 control-label">Nome:</label>
         <div class="col-sm-10">
-            <input type="text" name="name" class="form-control" id="name" placeholder="Nome">
+            <input type="text" name="name"  class="form-control" id="name"  placeholder="Nome">
         </div>
     </div>
     <div class="form-group">
         <label for="email" class="col-sm-2 control-label">E-mail</label>
         <div class="col-sm-10">
             <input type="email" name="email" class="form-control" id="email" placeholder="E-mail">
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="assunto" class="col-sm-2 control-label">Assunto</label>
+        <div class="col-sm-10">
+            <input type="text" name="subject" class="form-control" id="subject"  placeholder="Assunto">
         </div>
     </div>
     <div class="form-group">
@@ -34,7 +88,7 @@ endif;
     <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10">
             <button type="submit" class="btn btn-default">Enviar Contato</button>
-            <input type="hidden" name="hidden" id="hidden"/>
+            <input type="hidden" name="hidden" value = "submited" id="hidden"/>
         </div>
     </div>
 </form>
--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         <style type="text/css">
 
 
-            .nav > ul {
+            .nav > ul, .footer {
                 background:grey; 
                 padding: 15px;
                 border-radius:5px;
@@ -34,53 +34,84 @@
                 text-decoration: none;
                 color: #515151;
             }
+            .content {
+                min-height: 450px;
+                border: 1px solid #eee;
+                padding: 20px;
+                margin-top: 10px;
+                margin-bottom: 10px;
+            }
+
+            .footer > p {
+                color: white;
+            }
+
 
 
         </style>
 
     </head>
     <body>
+
         <div class="container-fluid">
 
             <div class="container">
 
-                <h1 class="text-center">Projeto de Exemplo 1</h1>
+                <header>
 
-                <div class="nav">
-                    <ul class="list-unstyled list-inline">
-                        <li><a href="?uri=home"><i class="fa fa-home"></i> Home</a></li>
-                        <li><a href="?uri=empresa"><i class="fa fa-building"></i> Empresa</a></li>
-                        <li><a href="?uri=produtos"><i class="fa fa-asterisk"></i> Produtos</a></li>
-                        <li><a href="?uri=servicos"><i class="fa fa-asterisk"></i> Servicos</a></li>
-                        <li><a href="?uri=contato"><i class="fa fa-asterisk"></i> Contato</a></li>
-                    </ul>
-                </div>
+                    <h1 class="text-center">Projeto de Exemplo 1</h1>
 
+                    <div class="nav">
+                        <ul class="list-unstyled list-inline">
+                            <li><a href="?uri=home"><i class="fa fa-home"></i> Home</a></li>
+                            <li><a href="?uri=empresa"><i class="fa fa-building"></i> Empresa</a></li>
+                            <li><a href="?uri=produtos"><i class="fa fa-asterisk"></i> Produtos</a></li>
+                            <li><a href="?uri=servicos"><i class="fa fa-asterisk"></i> Servicos</a></li>
+                            <li><a href="?uri=contato"><i class="fa fa-asterisk"></i> Contato</a></li>
+                        </ul>
+                    </div>
 
-                <div class="content">
-                    <?php
-                    //Filter GET Input
-                    $Uri = (filter_input(INPUT_GET, 'uri', FILTER_SANITIZE_STRING) ? filter_input(INPUT_GET, 'uri', FILTER_SANITIZE_STRING) : null);
-
-                    //Verify Conditional
-                    if (isset($Uri) && file_exists('inc/' . $Uri . '.php')):
-
-                        //Add file
-                        require_once('inc/' . $Uri . '.php');
-
-                    else:
-
-                        require_once('inc/home.php');
-
-                    endif;
-                    ?>
+                </header>
 
 
-                </div>
+                <main>
 
+                    <div class="content">
+                        <?php
+                        //Filter GET Input
+                        $Uri = (filter_input(INPUT_GET, 'uri', FILTER_SANITIZE_STRING) ? filter_input(INPUT_GET, 'uri', FILTER_SANITIZE_STRING) : null);
+
+                        //Verify Conditional
+                        if (isset($Uri) && file_exists('inc/' . $Uri . '.php')):
+
+                            //Add file
+                            require_once('inc/' . $Uri . '.php');
+
+                        else:
+
+                            require_once('inc/home.php');
+
+                        endif;
+                        ?>
+
+
+                    </div>
+
+                </main>
             </div>
         </div>
 
+        <footer>
+            <div class="container-fluyd">
+                <div class="container">
+                    <div class="footer">
+
+                        <p class="text-center">Todos os direitos reservados <?php echo date('d/m/Y'); ?></p>
+
+                    </div>
+                </div>
+            </div>
+        </footer>
 
         <script src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity = "sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin = "anonymous"></script>
     </body>
